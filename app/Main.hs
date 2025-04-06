5596deccbdf6444bf436190f461510de050b5d1c
Sean D Gillespie <sean@mistersg.net>
2025-04-05 23:07:59 -0400

    chore(app): Add a basic options parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,51 @@
 module Main where
 
-import MyLib qualified (someFunc)
+import Options.Applicative (Parser)
+import Options.Applicative qualified as Opts
+
+data Options = Options
+  { optInputFile :: InputFile,
+    optOutputFile :: OutputFile
+  }
+
+newtype InputFile = InputFile {unInputFile :: FilePath}
+  deriving stock (Eq, Show)
+
+newtype OutputFile = OutputFile {unOutputFile :: FilePath}
+  deriving stock (Eq, Show)
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = run =<< Opts.execParser parserInfo
+  where
+    parserInfo = Opts.info (parseOpts <**> Opts.helper) desc
+    desc = 
+      Opts.fullDesc
+        <> Opts.progDesc "Compile untitled programs"
+
+run :: Options -> IO ()
+run _ = pure ()
+
+parseOpts :: Parser Options
+parseOpts =
+  Options
+    <$> parseInputFile
+    <*> parseOutputFile
+
+parseInputFile :: Parser InputFile
+parseInputFile = InputFile <$> Opts.strArgument optMods
+  where
+    optMods =
+      Opts.metavar "<file>"
+        <> Opts.help "The input file to compile."
+
+parseOutputFile :: Parser OutputFile
+parseOutputFile = OutputFile <$> Opts.strOption optMods
+  where
+    optMods =
+      Opts.long "output"
+        <> Opts.short 'o'
+        <> Opts.metavar "<file>"
+        <> Opts.value "main"
+        <> Opts.showDefault
+        <> Opts.help "Specify the output file."
+
